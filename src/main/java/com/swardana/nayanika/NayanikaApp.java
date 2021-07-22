3d689f9a0f28f18589d4848c472fc58a05a476a6
Sukma Wardana <swardana@tutanota.com>
2021-07-22 13:44:22 +0700

    Add JavaFX libraries

---
+++ src/main/java/com/swardana/nayanika/NayanikaApp.java
@@ -0,0 +1,40 @@
+/*
+ * Nayanika, picture viewer application
+ * Copyright (C) 2021  Sukma Wardana
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.swardana.nayanika;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * The bootstrap class for Nayanika application.
+ *
+ * @author Sukma Wardana
+ */
+public class NayanikaApp extends Application {
+
+    @Override
+    public final void start(final Stage stage) throws Exception {
+        var scene = new Scene(new StackPane(new Label("Hello!")), 600, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
