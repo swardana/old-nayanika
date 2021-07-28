418bd817872aa9e1e1aaf0ce4af58d194d802fdf
Sukma Wardana <swardana@tutanota.com>
2021-07-28 13:14:59 +0700

    Add Presentation API contract

---
+++ src/main/java/com/swardana/nayanika/base/Presentation.java
@@ -0,0 +1,35 @@
+/*
+ * Nayanika, picture viewer application
+ * Copyright (C) 2021  Sukma Wardana
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.swardana.nayanika.base;
+
+/**
+ * A slide-show presentation.
+ *
+ * @author Sukma Wardana
+ */
+public interface Presentation {
+
+    /**
+     * Check whether the presentation is running or not.
+     *
+     * @return {@code true} if the slide-show presentation is running.
+     */
+    boolean isRunning();
+
+}
